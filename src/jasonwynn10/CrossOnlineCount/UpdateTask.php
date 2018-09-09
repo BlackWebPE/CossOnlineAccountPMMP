f13c3f775cd6f1cd7d432e7c02f706c891bb09b2
BlackWebPE <43094955+BlackWebPE@users.noreply.github.com>
2018-09-09 16:44:58 -0400

    Create UpdateTask.php

---
+++ src/jasonwynn10/CrossOnlineCount/UpdateTask.php
@@ -0,0 +1,8 @@
+<?php
+namespace jasonwynn10\CrossOnlineCount;
+use pocketmine\scheduler\PluginTask;
+class UpdateTask extends PluginTask {
+	public function onRun(int $currentTick) {
+		$this->getOwner()->update();
+	}
+}
